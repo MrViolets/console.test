27247a0975e53a1c1df084fdcff7ce5296c5d2d9
Sylvester <violetsmyster@gmail.com>
2024-08-20 21:59:30 +0100

    Update

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-// Attach an event listener to the button to test console.test
+'use strict'
+
 document.getElementById('testButton').addEventListener('click', function () {
   console.test('Button clicked! This is a test message.');
+});
+
+document.getElementById('logOnceButton').addEventListener('click', function () {
+  console.logOnce('Button clicked! This is a test message.');
 });
