a40005fc400b6979303fd3b834104e7cf3cd31c3
Sylvester <violetsmyster@gmail.com>
2024-08-20 19:43:40 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Attach an event listener to the button to test console.test
+document.getElementById('testButton').addEventListener('click', function () {
+  console.test('Button clicked! This is a test message.');
+});
